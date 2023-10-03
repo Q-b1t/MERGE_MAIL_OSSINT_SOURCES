d4268393f4365a86a57df833ab28f35523c78b35
Q-b1t <ldhq_rhiq@protonmail.com>
2023-10-02 21:39:41 -0600

    improved logic

--- auxiliary_modules/import_data.py
+++ auxiliary_modules/import_data.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from termcolor import colored
+from collections import Counter
+import re 
+
 
 def read_text_file(filename):
     with open(filename,"r",encoding="utf-8") as f:
@@ -20,3 +24,84 @@ def normalize_gatherings(table,normalizing_columns = ["name","position","email"]
         if col in table.columns:
             table[col] = table[col].apply(lambda x: str(x).lower())
             table[col] = table[col].apply(lambda x: x.replace("nan",""))
+
+# validate if the files where passed as parameters
+def get_metadata(leaks,list,gatherings):
+    inputs = [leaks,list,gatherings]
+    keys = ["leaks","email_list","gatherings"]
+    metadata = []
+    # initialize metadata dictionary
+    for inpt,key in zip(inputs,keys):
+        sample = dict()
+        if inpt is not None:
+            sample["name"] = inpt
+            sample["present"] = True
+        else:
+            sample["name"] = None
+            sample["present"] = False
+        sample["type"]= key
+        metadata.append(sample)
+    return metadata
+
+def validate_name(filename):
+    pattern = r"[A-Za-z0-9-_]{3,20}.(xlsx | csv | txt)"
+    assert re.match(pattern,filename),colored(f"[-] {filename} name is not valid. Please limit to numbers, lower and upper case letters, and special characters: \"-\",\"_\". Please do not include the file extention.","red")
+
+# validate if the files have the propper format and that there are at least two files
+def validate_metadata(metadata):
+    metadata = pd.DataFrame(metadata)
+    counts = metadata["present"].value_counts()
+    assert counts[True] >= 2, colored("The program requires at least 2 files in order to perform a merge operation.","red")
+
+    #for name in metadata["name"]:
+        #if name is None:
+            #continue
+        #validate_name(name)
+
+def fetch_data(metadata):
+    contents = dict()
+    for sample in metadata:
+        if sample["present"]:
+            if sample["type"] == "email_list":
+                contents[sample["type"]] = get_mail_df(filename=sample["name"])
+            else:
+                contents[sample["type"]] = get_excel_table(filename=sample["name"])
+                assert "email" in contents[sample["type"]].columns and "name" in contents[sample["type"]], colored(f"The table in {sample['name']} has not the appropriate format. Please make sure the columns \"email\" and \"name\" are present.","red")
+                normalize_gatherings(contents[sample["type"]])
+        else:
+            continue 
+    return contents
+
+def save_table(parsed_table,save_path,output_format,verbose):
+    """
+    Inputs: 
+        - parsed_table: A processed pandas dataframe.
+        - save_path: The to which the table will be saved as an excel book.
+    """
+    if output_format == "csv":
+        filename = save_path + "." + "csv"
+        parsed_table.to_csv(filename,index = False)
+    else:
+        filename = save_path + "." + "xlsx"
+        parsed_table.to_excel(filename, index=False)
+         
+    if verbose:
+        print(colored(f"[+] Breached data saved to {filename}","green"))
+
+def merge_tables(data_dumps,metadata):
+    leaks_present, mails_present,gatherings_present = metadata[0]["present"], metadata[1]["present"], metadata[2]["present"]
+    if leaks_present and mails_present and gatherings_present:
+        leaks_table,mails_table,gatherings_table = data_dumps["leaks"],data_dumps["email_list"],data_dumps["gatherings"]
+        merged_table = leaks_table.merge(mails_table,how="outer",on="email")
+        merged_table = merged_table.merge(gatherings_table,how="outer",on="email")
+    elif leaks_present and mails_present and not gatherings_present:
+        leaks_table,mails_table = data_dumps["leaks"],data_dumps["email_list"]
+        merged_table = leaks_table.merge(mails_table,how="outer",on="email")
+    elif leaks_present and not mails_present and gatherings_present:
+        leaks_table,gatherings_table = data_dumps["leaks"],data_dumps["gatherings"]
+        merged_table = leaks_table.merge(gatherings_table,how="outer",on="email")
+    else:
+        mails_table,gatherings_table = data_dumps["email_list"],data_dumps["gatherings"]
+        merged_table = mails_table.merge(gatherings_table,how="outer",on="email")
+    return merged_table
+        
--- main.py
+++ main.py
@@ -8,9 +8,13 @@ from argparse import ArgumentParser,Namespace
 if __name__ == '__main__':
     # create cli
     parser = ArgumentParser()
-    parser.add_argument("-m","--mails_list",help="A text file containing a list of emails (default: \"./inputs/mail_dump.txt\")",type=str,default="./inputs/mail_dump.txt",nargs="?")
-    parser.add_argument("-g","--manual_gatherings",help="A excel book containing manual gatherings of employees and emails (default: \"./inputs/manual_gatherings.xlsx\")",type=str,default="./inputs/manual_gatherings.xlsx",nargs="?")
-    parser.add_argument("-l","--leaks_table",help="An excel book containing emails leaks (default: \"./inputs/dehashed_dump.xlsx\")",type=str,default="./inputs/dehashed_dump.xlsx",nargs="?")
+    parser.add_argument("-m","--mails_list",help="A text file containing a list of emails",type=str,default=None,nargs="?")
+    parser.add_argument("-g","--manual_gatherings",help="A excel book containing manual gatherings of employees and emails",type=str,default=None,nargs="?")
+    parser.add_argument("-l","--leaks_table",help="An excel book containing emails leaks",type=str,default=None,nargs="?")
+    parser.add_argument("-v","--verbose",help="Whether to output information on the script's progress in the console",type=bool,default=False,nargs="?")
+    parser.add_argument("-o","--output_file",help="Name of the output file (default: \"merged_data\")",type=str,default="merged_data",nargs="?")
+    parser.add_argument("-f","--output_format",help="It can be either \"excel\" or \"csv\" (default: \"excel\")",type=str,default="excel",nargs="?")
+
 
 
     # fetch arguments
@@ -20,12 +24,22 @@ if __name__ == '__main__':
     mails_text_file_path = args.mails_list
     leaks_excel_file_path = args.leaks_table
     manual_gatherings_excel_file_path = args.manual_gatherings
+    merged_data_file_path = args.output_file
+    verbose = args.verbose
+    output_format = args.output_format
 
-    # output file
-    merged_data_file_path = "./outputs/merged_data.xlsx"
-    # output files
-    mail_text_file_dump = get_mail_df(filename=mails_text_file_path)
+    # validate the files that exists
+    metadata = get_metadata(leaks=leaks_excel_file_path,list=mails_text_file_path,gatherings=manual_gatherings_excel_file_path)
+    validate_metadata(metadata=metadata)
+    data_dumps = fetch_data(metadata=metadata)
+    merge_tables(data_dumps=data_dumps,metadata=metadata)
+    merged_table = merge_tables(metadata=metadata,data_dumps=data_dumps)
+    save_table(parsed_table=merged_table,save_path=merged_data_file_path,output_format=output_format,verbose=verbose)    
+
+    """
+    # read values
     leaks_dump = get_excel_table(filename=leaks_excel_file_path)
+    mail_text_file_dump = get_mail_df(filename=mails_text_file_path)
     manual_gatherings_dump = get_excel_table(filename=manual_gatherings_excel_file_path)
 
     # normalize the values if the string columns to merge as much data as possible
@@ -49,3 +63,4 @@ if __name__ == '__main__':
         merged_leaks_mails_gatherings.rename(columns={"name_x": "name"},inplace=True)
     # export
     merged_leaks_mails_gatherings.to_excel(merged_data_file_path)
+    """
