dec4cdbe40371cb409bb4a76b50e0000cb85338f
Q-b1t <ldhq_rhiq@protonmail.com>
2023-10-03 11:02:16 -0600

    adapted to receibe a any of the 3 sources

--- auxiliary_modules/import_data.py
+++ auxiliary_modules/import_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from termcolor import colored
 from collections import Counter
-import re 
+import os
 
 
 def read_text_file(filename):
@@ -43,20 +43,27 @@ def get_metadata(leaks,list,gatherings):
         metadata.append(sample)
     return metadata
 
-def validate_name(filename):
-    pattern = r"[A-Za-z0-9-_]{3,20}.(xlsx | csv | txt)"
-    assert re.match(pattern,filename),colored(f"[-] {filename} name is not valid. Please limit to numbers, lower and upper case letters, and special characters: \"-\",\"_\". Please do not include the file extention.","red")
+def validate_output_file(output_file):
+    # validate if outfile file contains no extentions
+    assert "xlsx" not in output_file or  "csv" not in output_file or "txt" not in output_file, colored(f"[-] The output file should only be limited to the name. Please ommit the extention.","red")
+    if output_file.find("/") != -1 or output_file.find("\\") != -1:
+        path = os.path.dirname(output_file)
+        assert os.path.exists(path=path), colored(f"The path {path} does not seem to exist.","red")
+
+def validate_name(filename,verbose):
+    assert  os.path.isfile(filename),colored(f"[-] Specified file {filename} does not seem to exist. Please check for any errors.","red")
+    if verbose:
+        print(colored(f"[+] File {filename} found.","green"))
 
 # validate if the files have the propper format and that there are at least two files
-def validate_metadata(metadata):
+def validate_metadata(metadata,verbose):
     metadata = pd.DataFrame(metadata)
     counts = metadata["present"].value_counts()
     assert counts[True] >= 2, colored("The program requires at least 2 files in order to perform a merge operation.","red")
-
-    #for name in metadata["name"]:
-        #if name is None:
-            #continue
-        #validate_name(name)
+    for name in metadata["name"]:
+        if name is None:
+            continue
+        validate_name(name,verbose) 
 
 def fetch_data(metadata):
     contents = dict()
@@ -88,20 +95,37 @@ def save_table(parsed_table,save_path,output_format,verbose):
     if verbose:
         print(colored(f"[+] Breached data saved to {filename}","green"))
 
-def merge_tables(data_dumps,metadata):
+def merge_tables(data_dumps,metadata,verbose):
     leaks_present, mails_present,gatherings_present = metadata[0]["present"], metadata[1]["present"], metadata[2]["present"]
     if leaks_present and mails_present and gatherings_present:
+        if verbose:
+            print(colored("[+] Merging data from all sources.","green"))
         leaks_table,mails_table,gatherings_table = data_dumps["leaks"],data_dumps["email_list"],data_dumps["gatherings"]
         merged_table = leaks_table.merge(mails_table,how="outer",on="email")
         merged_table = merged_table.merge(gatherings_table,how="outer",on="email")
     elif leaks_present and mails_present and not gatherings_present:
+        if verbose:
+            print(colored("[+] Merging mail list with leaks table.","green"))
         leaks_table,mails_table = data_dumps["leaks"],data_dumps["email_list"]
         merged_table = leaks_table.merge(mails_table,how="outer",on="email")
     elif leaks_present and not mails_present and gatherings_present:
+        if verbose:
+            print(colored("[+] Merging leaks and gatherings table.","green"))
         leaks_table,gatherings_table = data_dumps["leaks"],data_dumps["gatherings"]
         merged_table = leaks_table.merge(gatherings_table,how="outer",on="email")
     else:
+        if verbose:
+            print(colored("[+] Merging mail list with gatherings table.","green"))
         mails_table,gatherings_table = data_dumps["email_list"],data_dumps["gatherings"]
         merged_table = mails_table.merge(gatherings_table,how="outer",on="email")
+    # clean data
+    if "name_x" in merged_table.columns:
+        merged_table["name_x"].update(merged_table["name_y"])
+        merged_table.drop("name_y",axis=1,inplace=True)
+        merged_table.rename(columns={"name_x": "name"},inplace=True)
+    # remove trash columns
+    for column in merged_table.columns:
+        if "Unnamed" in column:
+            merged_table.drop(column,axis=1, inplace=True)
     return merged_table
         
--- main.py
+++ main.py
@@ -29,38 +29,13 @@ if __name__ == '__main__':
     output_format = args.output_format
 
     # validate the files that exists
+
     metadata = get_metadata(leaks=leaks_excel_file_path,list=mails_text_file_path,gatherings=manual_gatherings_excel_file_path)
-    validate_metadata(metadata=metadata)
+    if verbose:
+        print(colored(f"[+] Validating input and output files.","blue"))
+    validate_metadata(metadata=metadata,verbose=verbose)
+    validate_output_file(merged_data_file_path)
     data_dumps = fetch_data(metadata=metadata)
-    merge_tables(data_dumps=data_dumps,metadata=metadata)
-    merged_table = merge_tables(metadata=metadata,data_dumps=data_dumps)
+    merge_tables(data_dumps=data_dumps,metadata=metadata,verbose=verbose)
+    merged_table = merge_tables(metadata=metadata,data_dumps=data_dumps,verbose=verbose)
     save_table(parsed_table=merged_table,save_path=merged_data_file_path,output_format=output_format,verbose=verbose)    
-
-    """
-    # read values
-    leaks_dump = get_excel_table(filename=leaks_excel_file_path)
-    mail_text_file_dump = get_mail_df(filename=mails_text_file_path)
-    manual_gatherings_dump = get_excel_table(filename=manual_gatherings_excel_file_path)
-
-    # normalize the values if the string columns to merge as much data as possible
-    normalize_gatherings(manual_gatherings_dump)
-    normalize_gatherings(leaks_dump)
-    
-    # merge the leaks data with the raw mail dump
-    merged_leaks_mails = leaks_dump.merge(mail_text_file_dump,how="outer",on="email")
-    # meget the merged data with the manual gatherings
-    merged_leaks_mails_gatherings = merged_leaks_mails.merge(manual_gatherings_dump,how="outer",on="email")
-
-    # remove redundant columns
-    for col in merged_leaks_mails_gatherings.columns:
-        if "Unnamed" in col:
-            merged_leaks_mails_gatherings.drop(col,axis=1, inplace=True)
-
-    
-    if "name_x" in merged_leaks_mails_gatherings.columns:
-        merged_leaks_mails_gatherings["name_x"].update(merged_leaks_mails_gatherings["name_y"])
-        merged_leaks_mails_gatherings.drop("name_y",axis=1,inplace=True)
-        merged_leaks_mails_gatherings.rename(columns={"name_x": "name"},inplace=True)
-    # export
-    merged_leaks_mails_gatherings.to_excel(merged_data_file_path)
-    """
